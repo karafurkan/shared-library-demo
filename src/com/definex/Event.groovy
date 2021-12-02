311661b591e19272ee762bbc4e33e041407bb06c
Ege Kocabas <ege@kodgemisi.com>
2021-12-02 20:53:06 +0300

    commit

---
+++ src/com/definex/Event.groovy
@@ -0,0 +1,10 @@
+package com.definex
+
+/**
+ * Created on December, 2021
+ *
+ * @author ege
+ */
+class Event {
+    public static final String SLACK_MESSAGE_NEW = "Sending Slack Notification... with using anoth";
+}
