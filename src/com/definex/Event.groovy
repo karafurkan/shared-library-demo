ee01b3b4db4a72cda4a08fe746a310ade31293ed
Ege Kocabas <ege@kodgemisi.com>
2021-12-02 20:56:10 +0300

    commit

--- src/com/definex/Event.groovy
+++ src/com/definex/Event.groovy
@@ -11,4 +11,17 @@ class Event {
     public static final String SLACK_MESSAGE_NEW = "Sending Slack Notification... with using anoth eveeent";
 
     private static Map<String, List<EventSubscription>> eventMap = new HashMap<>();
+
+
+    /**
+     * Subscribe to an event so that the provided closure will be executed when the event is published.
+     * @param eventName Event name to subscribe to
+     * @param failPipeline Fail the pipeline if any errors occur in the closure
+     * @param closure Closure to execute
+     */
+    public static void subscribe(String eventName, boolean failPipeline, Closure closure) {
+        //TODO
+    }
+
+
 }
