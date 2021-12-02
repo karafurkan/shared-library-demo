820785fd56f9cfd89576ab50ac70472ea7c671db
Ege Kocabas <ege@kodgemisi.com>
2021-12-02 21:43:03 +0300

    commit

--- vars/log.groovy
+++ vars/log.groovy
@@ -7,13 +7,6 @@
  */
 
 
-
-def call(){
-    //script.log.warning("inside action configuration validation error has occurred: ")
-    println "log call"
-}
-
 def warning(String logMessage){
-    //script.log.warning("inside action configuration validation error has occurred: ")
-    println "log denemeeeeeee ${logMessage}"
+    println "${logMessage}"
 }
