687eb75681abc82de97d156de65a4ad7e3e412bf
Ege Kocabas <ege@kodgemisi.com>
2021-12-02 20:08:19 +0300

    commit

--- src/com/definex/AnotherConst.groovy
+++ src/com/definex/AnotherConst.groovy
@@ -2,17 +2,22 @@ package com.definex
 
 import com.definex.Constants
 
+import java.util.logging.Logger
 /**
  * Created on December, 2021
  *
  * @author ege
  */
 class AnotherConst {
+
+    private static Logger logger = Logger.getLogger(AnotherConst.getName())
+
     public static final String SLACK_MESSAGE_NEW = "Sending Slack Notification... with using another class static ${Constants.EMAIL_MESSAGE}"
 
 
     public static void deneme(){
-        println "aaaaaaaaaaaaaaaaaa"
+        logger.info('Hello from a Job DSL script!')
+        //println "aaaaaaaaaaaaaaaaaa"
     }
 
 }
